fnordahl/ovn

commit e3a398e9146eb889a339fd9d75cf86a94093782b

controller: Add ipv6 prefix delegation state machine
Introduce IPv6 Prefix delegation state machine according to RFC 3633
https://tools.ietf.org/html/rfc3633.
Add handle_dhcpv6_reply controller action to parse advertise/reply from
IPv6 delegation server. Advertise/reply are parsed running respectively:
- pinctrl_parse_dhcv6_advt
- pinctrl_parse_dhcv6_reply
The IPv6 requesting router starts sending dhcpv6 solicit through the logical
router port marked with ipv6_prefix_delegation set to true.
An IPv6 prefix will be requested for each logical router port marked
with "prefix" set to true in option column of logical router port table.
Save IPv6 prefix received by IPv6 delegation router in the options columns of
SB port binding table in order to be reused by Router Advertisement framework
run by ovn logical router pipeline.
IPv6 Prefix delegation state machine is enabled on Gateway Router or on
a Gateway Router Port

Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

lib/ovn-l7.h
@@ -178,8 +178,11 @@ struct dhcp_opt6_header {
 #define DHCPV6_OPT_SERVER_ID_CODE        2
 #define DHCPV6_OPT_IA_NA_CODE            3
 #define DHCPV6_OPT_IA_ADDR_CODE          5
+#define DHCPV6_OPT_STATUS_CODE           13
 #define DHCPV6_OPT_DNS_SERVER_CODE       23
 #define DHCPV6_OPT_DOMAIN_SEARCH_CODE    24
+#define DHCPV6_OPT_IA_PD                 25
+#define DHCPV6_OPT_IA_PREFIX             26
 
 #define DHCPV6_OPT_SERVER_ID \
     DHCP_OPTION("server_id", DHCPV6_OPT_SERVER_ID_CODE, "mac")
@@ -257,6 +260,22 @@ struct ovs_nd_route_info {
     ovs_be32 route_lifetime;
 };
 BUILD_ASSERT_DECL(ND_ROUTE_INFO_OPT_LEN == sizeof(struct ovs_nd_route_info));
+
+OVS_PACKED(
+struct dhcpv6_opt_ia_prefix {
+    struct dhcpv6_opt_header opt;
+    ovs_be32 plife_time;
+    ovs_be32 vlife_time;
+    uint8_t plen;
+    struct in6_addr ipv6;
+});
+
+OVS_PACKED(
+struct dhcpv6_opt_status {
+    struct dhcpv6_opt_header opt;
+    ovs_be16 status_code;
+    uint8_t msg[];
+});
 
 #define DHCPV6_DUID_LL      3
 #define DHCPV6_HW_TYPE_ETH  1

include/ovn/actions.h
@@ -91,7 +91,8 @@ struct ovn_extend_table;
     OVNACT(TRIGGER_EVENT,     ovnact_controller_event) \
     OVNACT(BIND_VPORT,        ovnact_bind_vport)       \
     OVNACT(HANDLE_SVC_CHECK,  ovnact_handle_svc_check) \
-    OVNACT(FWD_GROUP,         ovnact_fwd_group)
+    OVNACT(FWD_GROUP,         ovnact_fwd_group)       \
+    OVNACT(DHCP6_REPLY,       ovnact_null)
 
 /* enum ovnact_type, with a member OVNACT_<ENUM> for each action. */
 enum OVS_PACKED_ENUM ovnact_type {
@@ -584,6 +585,11 @@ enum action_opcode {
      *     MFF_LOG_INPORT = port
      */
     ACTION_OPCODE_HANDLE_SVC_CHECK,
+    /* handle_dhcpv6_reply { ...actions ...}."
+     *
+     *  The actions, in OpenFlow 1.3 format, follow the action_header.
+     */
+    ACTION_OPCODE_DHCP6_SERVER,
 };
 
 /* Header. */

lib/actions.c
@@ -2362,6 +2362,20 @@ ovnact_put_opts_free(struct ovnact_put_opts *pdo)
 }
 
 static void
+format_DHCP6_REPLY(const struct ovnact_null *a OVS_UNUSED, struct ds *s)
+{
+    ds_put_cstr(s, "handle_dhcpv6_reply;");
+}
+
+static void
+encode_DHCP6_REPLY(const struct ovnact_null *a OVS_UNUSED,
+                   const struct ovnact_encode_params *ep OVS_UNUSED,
+                   struct ofpbuf *ofpacts)
+{
+    encode_controller_op(ACTION_OPCODE_DHCP6_SERVER, ofpacts);
+}
+
+static void
 parse_SET_QUEUE(struct action_context *ctx)
 {
     int queue_id;
@@ -3308,6 +3322,8 @@ parse_action(struct action_context *ctx)
         parse_handle_svc_check(ctx);
     } else if (lexer_match_id(ctx->lexer, "fwd_group")) {
         parse_fwd_group_action(ctx);
+    } else if (lexer_match_id(ctx->lexer, "handle_dhcpv6_reply")) {
+        ovnact_put_DHCP6_REPLY(ctx->ovnacts);
     } else {
         lexer_syntax_error(ctx->lexer, "expecting action");
     }

ovn-sb.xml
@@ -2149,6 +2149,17 @@ tcp.flags = RST;
           <p><b>Example:</b> <code>handle_svc_check(inport);</code></p>
         </dd>
 
+        <dt><code>handle_dhcpv6_reply;</code></dt>
+        <dd>
+          <p>
+            Handle DHCPv6 prefix delegation advertisements/replies from
+            a IPv6 delegation server. <code>ovn-controller</code> will
+            add an entry <code>ipv6_ra_pd_list</code> in the
+            <ref table="Port_Binding" column="options"/> table for each
+            prefix received from the delegation server
+          </p>
+        </dd>
+
         <dt><code><var>R</var> = select(<var>N1</var>[=<var>W1</var>], <var>N2</var>[=<var>W2</var>], ...);</code></dt>
         <dd>
           <p>
@@ -2182,6 +2193,13 @@ tcp.flags = RST;
           </p>
         </dd>
 
+        <dt><code>handle_dhcpv6_reply;</code></dt>
+        <dd>
+          <p>
+            This action is used to parse DHCPv6 replies from IPv6
+            Delegation Router and managed IPv6 Prefix delegation state machine
+          </p>
+        </dd>
       </dl>
     </column>
 

tests/ovn.at
@@ -1554,6 +1554,10 @@ fwd_group();
 fwd_group(liveness="false", childports="eth0", "lsp1");
     Syntax error at `"false"' expecting `,'.
 
+# prefix delegation
+handle_dhcpv6_reply;
+    encodes as controller(userdata=00.00.00.13.00.00.00.00)
+
 # Miscellaneous negative tests.
 ;
     Syntax error at `;'.

utilities/ovn-trace.c
@@ -2292,6 +2292,8 @@ trace_actions(const struct ovnact *ovnacts, size_t ovnacts_len,
 
         case OVNACT_FWD_GROUP:
             break;
+        case OVNACT_DHCP6_REPLY:
+            break;
         }
     }
     ds_destroy(&s);

commit 7c54e21da19bc93f351283421fe14476ac04b21d

controller: Use multicast for IPv6 Prefix Delegation.
The OVN Controller currently uses unicast messages when
communicating with the DHCPv6 server.

This is at odds with RFC 3315 paragraph 1 in each of the sections
18.2.1, 18.2.3, 18.2.6, and 18.2.7.

The client is expected to use multicast, unless the server has
explicitly told it to use unicast using the Server Unicast
Option [0].  I contemplated adding support for this option, but
I found no server implementation that made use of it.

Other supporting documentation is a change to ISC DHCP server
making it reject unicast Request, Renew, Decline and Release
messages [1].  The dnsmasq implementation also enforces the use
of multicast messages [2].

Fix the ovn-controller IPv6 Prefix Delegation implementation by
making it send requests using the all DHCP agents multicast
address [3] as expected.

0: https://datatracker.ietf.org/doc/html/rfc3315#section-22.12
1: https://github.com/isc-projects/dhcp/commit/b31fe1d552
2: https://github.com/imp/dnsmasq/blob/770bce967c/src/rfc3315.c#L347-L357
3: https://www.rfc-editor.org/rfc/rfc2375.html
Fixes: e3a398e9146e ("controller: Add ipv6 prefix delegation state machine")
Signed-off-by: Frode Nordahl <fnordahl@ubuntu.com>

controller/pinctrl.c
@@ -758,9 +758,6 @@ struct ipv6_prefixd_state {
     long long int next_announce;
     long long int last_complete;
     long long int last_used;
-    /* IPv6 PD server info */
-    struct in6_addr server_addr;
-    struct eth_addr sa;
     /* server_id_info */
     struct {
         uint8_t data[DHCPV6_MAX_DUID_LEN];
@@ -917,12 +914,13 @@ pinctrl_parse_dhcpv6_advt(struct rconn *swconn, const struct flow *ip_flow,
     struct dp_packet packet;
 
     dp_packet_use_stub(&packet, packet_stub, sizeof packet_stub);
-    eth_compose(&packet, ip_flow->dl_src, ip_flow->dl_dst, ETH_TYPE_IPV6,
-                IPV6_HEADER_LEN);
+    eth_compose(&packet, (struct eth_addr) ETH_ADDR_C(33,33,00,01,00,02),
+                ip_flow->dl_dst, ETH_TYPE_IPV6, IPV6_HEADER_LEN);
 
     struct udp_header *udp_h = compose_ipv6(&packet, IPPROTO_UDP,
                                             &ip_flow->ipv6_dst,
-                                            &ip_flow->ipv6_src, 0, 0, 255,
+                                            &in6addr_all_dhcp_agents,
+                                            0, 0, 255,
                                             len + UDP_HEADER_LEN + 4);
     udp_h->udp_len = htons(len + UDP_HEADER_LEN + 4);
     udp_h->udp_csum = 0;
@@ -976,7 +974,6 @@ out:
 static void
 pinctrl_prefixd_state_handler(const struct flow *ip_flow,
                               struct in6_addr addr, unsigned aid,
-                              struct eth_addr sa, struct in6_addr server_addr,
                               char prefix_len, unsigned t1, unsigned t2,
                               unsigned plife_time, unsigned vlife_time,
                               const uint8_t *uuid, uint8_t uuid_len)
@@ -986,8 +983,6 @@ pinctrl_prefixd_state_handler(const struct flow *ip_flow,
     pfd = pinctrl_find_prefixd_state(ip_flow, aid);
     if (pfd) {
         pfd->state = PREFIX_PENDING;
-        pfd->server_addr = server_addr;
-        pfd->sa = sa;
         memcpy(pfd->uuid.data, uuid, uuid_len);
         pfd->uuid.len = uuid_len;
         pfd->plife_time = plife_time * 1000;
@@ -1005,10 +1000,6 @@ pinctrl_parse_dhcpv6_reply(struct dp_packet *pkt_in,
                            const struct flow *ip_flow)
     OVS_REQUIRES(pinctrl_mutex)
 {
-    struct eth_header *eth = dp_packet_eth(pkt_in);
-    struct ovs_16aligned_ip6_hdr *in_ip = dp_packet_l3(pkt_in);
-    struct in6_addr ip6_src;
-    memcpy(&ip6_src, &in_ip->ip6_src, sizeof ip6_src);
     struct udp_header *udp_in = dp_packet_l4(pkt_in);
     unsigned char *in_dhcpv6_data = (unsigned char *)(udp_in + 1);
     size_t dlen = MIN(ntohs(udp_in->udp_len), dp_packet_l4_size(pkt_in));
@@ -1091,8 +1082,7 @@ pinctrl_parse_dhcpv6_reply(struct dp_packet *pkt_in,
             VLOG_DBG_RL(&rl, "Received DHCPv6 reply from %s with prefix %s/%d"
                         " aid %d", ip6_s, prefix, prefix_len, aid);
         }
-        pinctrl_prefixd_state_handler(ip_flow, ipv6, aid, eth->eth_src,
-                                      ip6_src, prefix_len, t1, t2,
+        pinctrl_prefixd_state_handler(ip_flow, ipv6, aid, prefix_len, t1, t2,
                                       plife_time, vlife_time, uuid, uuid_len);
     }
 }
@@ -1125,28 +1115,22 @@ pinctrl_handle_dhcp6_server(struct rconn *swconn, const struct flow *ip_flow,
 static void
 compose_prefixd_packet(struct dp_packet *b, struct ipv6_prefixd_state *pfd)
 {
-    struct in6_addr ipv6_dst;
-    struct eth_addr eth_dst;
-
     int payload = sizeof(struct dhcpv6_opt_server_id) +
                   sizeof(struct dhcpv6_opt_ia_na);
     if (pfd->uuid.len) {
         payload += pfd->uuid.len + sizeof(struct dhcpv6_opt_header);
-        ipv6_dst = pfd->server_addr;
-        eth_dst = pfd->sa;
-    } else {
-        eth_dst = (struct eth_addr) ETH_ADDR_C(33,33,00,01,00,02);
-        ipv6_parse("ff02::1:2", &ipv6_dst);
     }
     if (ipv6_addr_is_set(&pfd->prefix)) {
         payload += sizeof(struct dhcpv6_opt_ia_prefix);
     }
 
-    eth_compose(b, eth_dst, pfd->ea, ETH_TYPE_IPV6, IPV6_HEADER_LEN);
+    eth_compose(b, (struct eth_addr) ETH_ADDR_C(33,33,00,01,00,02), pfd->ea,
+                ETH_TYPE_IPV6, IPV6_HEADER_LEN);
 
     int len = UDP_HEADER_LEN + 4 + payload;
     struct udp_header *udp_h = compose_ipv6(b, IPPROTO_UDP, &pfd->ipv6_addr,
-                                            &ipv6_dst, 0, 0, 255, len);
+                                            &in6addr_all_dhcp_agents,
+                                            0, 0, 255, len);
     udp_h->udp_len = htons(len);
     udp_h->udp_csum = 0;
     packet_set_udp_port(b, htons(546), htons(547));

lib/ovn-l7.c
@@ -18,6 +18,8 @@
 
 #include "ovn-l7.h"
 
+const struct in6_addr in6addr_all_dhcp_agents = IN6ADDR_ALL_DHCP_AGENTS_INIT;
+
 bool
 ipv6_addr_is_routable_multicast(const struct in6_addr *ip)
 {

lib/ovn-l7.h
@@ -305,6 +305,12 @@ BUILD_ASSERT_DECL(DHCP_OPT_HEADER_LEN == sizeof(struct dhcp_opt_header));
 #define DHCPV6_FQDN_FLAGS_O 1 << 1
 #define DHCPV6_FQDN_FLAGS_N 1 << 2
 
+extern const struct in6_addr in6addr_all_dhcp_agents;
+#define IN6ADDR_ALL_DHCP_AGENTS_INIT { { { 0xff,0x02,0x00,0x00,0x00,0x00,     \
+                                           0x00,0x00,0x00,0x00,0x00,0x00,     \
+                                           0x00,0x01,0x00,0x02 } } }
+
+
 #define DHCP6_OPT_HEADER_LEN 4
 OVS_PACKED(
 struct dhcpv6_opt_header {

tests/system-common-macros.at
@@ -352,7 +352,7 @@ OVS_TRAFFIC_VSWITCHD_START()
 ADD_BR([br-int])
 ADD_BR([br-ext])
 
-ovs-ofctl add-flow br-ext action=normal
+ovs-vsctl set-fail-mode br-ext standalone
 # Set external-ids in br-int needed for ovn-controller
 ovs-vsctl \
         -- set Open_vSwitch . external-ids:system-id=hv1 \
@@ -426,7 +426,6 @@ ovn-nbctl --wait=hv sync
 cat > /etc/dhcp/dhcpd.conf <<EOF
 option dhcp-rebinding-time 10;
 option dhcp-renewal-time 5;
-option dhcp6.unicast 2001:1db8:3333::1;
 subnet6 2001:1db8:3333::/64 {
     prefix6 2001:1db8:3333:100:: 2001:1db8:3333:111:: /96;
 }